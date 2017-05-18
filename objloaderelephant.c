91804e2b444737eb44a5ad0f6c4205f82ed880e6
vitorfernando <vitorfernando42@hotmail.com>
2017-05-17 23:48:34 -0300

    Update objloaderelephant.c
    just fill ajusts

--- objloaderelephant.c
+++ objloaderelephant.c
@@ -14,7 +14,7 @@ GLuint model;
 GLfloat angle =0, fAspect;
 char ch='1';
 
-//------------ Funcs -------------- \\
+//------------ Funcs ----------------\\
 
 //read what key was pressed and moves the obj according to it
 void SpecialKeys(int key, int x, int y){
