a5678a1d3ab02715da7440b2a811a5b17168a9c6
vitorfernando <vitorfernando42@hotmail.com>
2017-05-21 05:54:57 -0300

    Update exec.c
    update code, using the modern openGL, this new code uses simple shaders, load a .obj file and your .bmp texture

--- exec.c
+++ exec.c
@@ -1,214 +1,203 @@
-//header
+// Include standard headers
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
 
-#include<GL/gl.h>
-#include<GL/glut.h>
-#include<stdio.h>
+// Include GLEW
+#include <GL/glew.h>
 
-//globals
-GLdouble obsX, obsY, obsZ, objScaleX, objScaleY, objScaleZ;//Initial position of observer and obj
-GLint windowWidth = 800, windowHeight = 600;
-GLuint model;
-GLfloat fov, fAspect;
-//------------ Funcs ----------------\\
+// Include GLFW
+#include <GLFW/glfw3.h>
+GLFWwindow* window;
 
-//set the default position
-void setDefaultPosition(){
-	obsX = 0;
-	obsY = 0;
-	obsZ = 0;
-	objScaleX = 0.1;
-	objScaleY = 0.1;
-	objScaleZ = 0.1;
-	fov = 60;
-}
+// Include GLM
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+using namespace glm;
 
-//other functions and main
+#include "shader.hpp"
+#include "texture.hpp"
+#include "objloader.hpp"
+#include "controls.hpp"
 
-//.obj loader code begins
-void loadObj(char *fname){
-	FILE *fp;
-	int read;
-	GLfloat x, y, z;
-	char ch;
-	model=glGenLists(1);
-	fp=fopen(fname,"r");
-	if (!fp) {
-		printf("can't open file %s\n", fname);
-		exit(1);
-	}
-	glPointSize(2.0);
-	glNewList(model, GL_COMPILE);
+int main( void )
+{
+	// Initialise GLFW
+	if( !glfwInit() )
 	{
-		glPushMatrix();
-		glBegin(GL_POINTS);
-		while(!(feof(fp))){
-			read=fscanf(fp,"%c %f %f %f",&ch,&x,&y,&z);
-			if(read==4&&ch=='v'){
-				glVertex3f(x,y,z);
-			}
-		}
-		glEnd();
+		fprintf( stderr, "Failed to initialize GLFW\n" );
+		getchar();
+		return -1;
 	}
-	glPopMatrix();
-	glEndList();
-	fclose(fp);
-}
 
-//.obj loader code ends here
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-void reshape(int w,int h){
-	    
-	glViewport(0,0,w,h);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	fAspect = (GLfloat)w / (GLfloat)h;
-    gluPerspective (60, fAspect, 0.1, 1000.0);
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-}
-
-void drawModel(){
-	
- 	glPushMatrix();
- 	glTranslatef(0,-20,-55);//model position on scene
- 	glColor3f(1.0,0,0);//model color
- 	glScalef(objScaleX, objScaleY, objScaleZ);//model scale
- 	glRotatef(obsX,1,0,0);//rotate model on X axis
- 	glRotatef(obsY,0,1,0);//rotate model on Y axis
- 	glRotatef(obsZ,0,0,1);//rotate model on Z axis
- 	glCallList(model);
- 	glPopMatrix();
-}
-
-void display(void)
-{
-   	glClearColor (0.0,0.0,0.0,1.0); 
-   	glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-   	glLoadIdentity();
-   	drawModel();
-   	glutSwapBuffers(); //swap the buffers
-
-}
-
-//defines the position of the observer and model
-void ViewerPosition(void)
-{
-	// Specifies model coordinates
-	glMatrixMode(GL_MODELVIEW);
-	// Initialize model coordinates
-	glLoadIdentity();
-	// Specifies observer and target position
-	// (eyeX,eyeY,eyeZ, centerX,centerY,centerZ, upX,upY,upZ)
-	gluLookAt(obsX,obsY,obsZ, 0,0,0, 0,1,0);
-}
-
-// Specifies the view volume
-void setProjection(void)
-{
-	// Specifies projection coordinates
-	glMatrixMode(GL_PROJECTION);
-	// Initialize projection coordinates
-	glLoadIdentity();
-	
-	// Specifies the projection perspective (angle,aspect,zNear,zFar)
-	//fovY is the angle, in degrees, on Y plan (it is used to set the "height" of the view volume);
-	//aspect determinas the view area on X plan, its value is determined by X and Y ratio; 
-	//zNear, always positive, is the distance from de observer to the closer cut plan;
-	//zFar, always positive, is the distance from the observer to the further cut plan.
-	gluPerspective(fov,fAspect,0.5,1000);
-
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-}
-
-//manage the events from mouse
-void MouseHandler(int button, int state, int x, int y)
-{
-	if (button == GLUT_LEFT_BUTTON)
-		if (state == GLUT_DOWN) {  // Zoom-in
-			if (fov >= 10) fov -= 5;
-		}
-	if (button == GLUT_RIGHT_BUTTON)
-		if (state == GLUT_DOWN) {  // Zoom-out
-			if (fov <= 130) fov += 5;
-		}
-	setProjection();
-	glutPostRedisplay();
-}
-
-
-//read what key was pressed and moves the obj according to it
-void specialKeys(int key, int x, int y){
-	switch (key) {
-		case GLUT_KEY_LEFT : 
-			obsY +=10;
-			break;
-		case GLUT_KEY_RIGHT : 
-			obsY -=10;
-			break;
-		case GLUT_KEY_UP : 
-			obsX +=10;
-			break;
-		case GLUT_KEY_DOWN : 
-			obsX -=10;
-			break;
-		case GLUT_KEY_HOME : 
-			obsZ +=10;
-			break;
-		case GLUT_KEY_END : 
-			obsZ -=10;
-			break;
+	// Open a window and create its OpenGL context
+	window = glfwCreateWindow( 1024, 768, "Tutorial 07 - Model Loading", NULL, NULL);
+	if( window == NULL ){
+		fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
+		getchar();
+		glfwTerminate();
+		return -1;
 	}
-	glLoadIdentity();
-	glutPostRedisplay();
-}
+	glfwMakeContextCurrent(window);
 
-
-//read alphanumeric inputs from keyboard
-void keyPressed(unsigned char key, int x, int y) {  
-	switch (key) {
-		case 'a' : 
-			objScaleX +=0.01;
-			break;
-		case 'd' : 
-			objScaleX -=0.01;
-			break;
-		case 'w' : 
-			objScaleY +=0.01;
-			break;
-		case 's' : 
-			objScaleY -=0.01;
-			break;
-		case 'q' : 
-			objScaleZ +=0.01;
-			break;
-		case 'e' : 
-			objScaleZ -=0.01;
-			break;
-		case ' ' : 
-			setDefaultPosition();
-			break;
+	// Initialize GLEW
+	glewExperimental = true; // Needed for core profile
+	if (glewInit() != GLEW_OK) {
+		fprintf(stderr, "Failed to initialize GLEW\n");
+		getchar();
+		glfwTerminate();
+		return -1;
 	}
-	glLoadIdentity();
-	glutPostRedisplay();
-}
 
-int main(int argc,char **argv){
+	// Ensure we can capture the escape key being pressed below
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+    // Hide the mouse and enable unlimited mouvement
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    
+    // Set the mouse at the center of the screen
+    glfwPollEvents();
+    glfwSetCursorPos(window, 1024/2, 768/2);
+
+	// Dark blue background
+	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+
+	// Enable depth test
+	glEnable(GL_DEPTH_TEST);
+	// Accept fragment if it closer to the camera than the former one
+	glDepthFunc(GL_LESS); 
+
+	// Cull triangles which normal is not towards the camera
+	glEnable(GL_CULL_FACE);
+
+	GLuint VertexArrayID;
+	glGenVertexArrays(1, &VertexArrayID);
+	glBindVertexArray(VertexArrayID);
+
+	// Create and compile our GLSL program from the shaders
+	GLuint programID = LoadShaders( "SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader" );
 	
-	glutInit(&argc,argv);
-	setDefaultPosition();
-	glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGB|GLUT_DEPTH);
-	glutInitWindowSize(windowWidth, windowHeight);
-	glutInitWindowPosition((glutGet(GLUT_SCREEN_WIDTH)-windowWidth) / 2, (glutGet(GLUT_SCREEN_HEIGHT)-windowHeight)/2);
-	glutCreateWindow("3D Visualizer");
-	glutReshapeFunc(reshape);
-	glutMouseFunc(MouseHandler);
-	glutSetCursor(GLUT_CURSOR_FULL_CROSSHAIR);
-    glutDisplayFunc(display);
-	glutIdleFunc(display);
-	glutSpecialFunc(specialKeys);
-	glutKeyboardFunc(keyPressed);
-    loadObj(argv[1]);//replace porsche.obj with radar.obj or any other .obj to display it
-	glutMainLoop();
+	// Get a handle for our "MVP" uniform
+	GLuint MatrixID = glGetUniformLocation(programID, "MVP");
+
+	// Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+	glm::mat4 Projection = glm::perspective(45.0f, 4.0f / 3.0f, 0.1f, 100.0f);
+	// Or, for an ortho camera :
+	//glm::mat4 Projection = glm::ortho(-10.0f,10.0f,-10.0f,10.0f,0.0f,100.0f); // In world coordinates
+	
+	// Camera matrix
+	glm::mat4 View = glm::lookAt(
+						glm::vec3(4,3,3), // Camera is at (4,3,3), in World Space
+						glm::vec3(0,0,0), // and looks at the origin
+						glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
+					);
+	// Model matrix : an identity matrix (model will be at the origin)
+	glm::mat4 Model = glm::mat4(1.0f);
+	// Our ModelViewProjection : multiplication of our 3 matrices
+	glm::mat4 MVP = Projection * View * Model; // Remember, matrix multiplication is the other way around
+
+	// Load the texture
+	GLuint Texture = loadDDS("uvmap.DDS");
+	
+	// Get a handle for our "myTextureSampler" uniform
+	GLuint TextureID  = glGetUniformLocation(programID, "myTextureSampler");
+
+	// Read our .obj file
+	std::vector<glm::vec3> vertices;
+	std::vector<glm::vec2> uvs;
+	std::vector<glm::vec3> normals; // Won't be used at the moment.
+	bool res = loadOBJ("cube.obj", vertices, uvs, normals);
+
+	// Load it into a VBO
+
+	GLuint vertexbuffer;
+	glGenBuffers(1, &vertexbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), &vertices[0], GL_STATIC_DRAW);
+
+	GLuint uvbuffer;
+	glGenBuffers(1, &uvbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
+	glBufferData(GL_ARRAY_BUFFER, uvs.size() * sizeof(glm::vec2), &uvs[0], GL_STATIC_DRAW);
+
+	do{
+
+		// Clear the screen
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// Use our shader
+		glUseProgram(programID);
+
+		// Compute the MVP matrix from keyboard and mouse input
+		//computeMatricesFromInputs();
+		//glm::mat4 ProjectionMatrix = getProjectionMatrix();
+		//glm::mat4 ViewMatrix = getViewMatrix();
+		//glm::mat4 ModelMatrix = glm::mat4(1.0);
+		//glm::mat4 MVP = ProjectionMatrix * ViewMatrix * ModelMatrix;
+		
+		// Send our transformation to the currently bound shader, 
+		// in the "MVP" uniform
+		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+
+		// Bind our texture in Texture Unit 0
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, Texture);
+		// Set our "myTextureSampler" sampler to user Texture Unit 0
+		glUniform1i(TextureID, 0);
+
+		// 1rst attribute buffer : vertices
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+		glVertexAttribPointer(
+			0,                  // attribute
+			3,                  // size
+			GL_FLOAT,           // type
+			GL_FALSE,           // normalized?
+			0,                  // stride
+			(void*)0            // array buffer offset
+		);
+
+		// 2nd attribute buffer : UVs
+		glEnableVertexAttribArray(1);
+		glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
+		glVertexAttribPointer(
+			1,                                // attribute
+			2,                                // size
+			GL_FLOAT,                         // type
+			GL_FALSE,                         // normalized?
+			0,                                // stride
+			(void*)0                          // array buffer offset
+		);
+
+		// Draw the triangle !
+		glDrawArrays(GL_TRIANGLES, 0, vertices.size() );
+
+		glDisableVertexAttribArray(0);
+		glDisableVertexAttribArray(1);
+
+		// Swap buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+	} // Check if the ESC key was pressed or the window was closed
+	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
+		   glfwWindowShouldClose(window) == 0 );
+
+	// Cleanup VBO and shader
+	glDeleteBuffers(1, &vertexbuffer);
+	glDeleteBuffers(1, &uvbuffer);
+	glDeleteProgram(programID);
+	glDeleteTextures(1, &TextureID);
+	glDeleteVertexArrays(1, &VertexArrayID);
+
+	// Close OpenGL window and terminate GLFW
+	glfwTerminate();
+
 	return 0;
 }
+
