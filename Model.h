cd80357a4af6889dfcb1dfa926d1924a74290827
vitorfernando <vitorfernando42@hotmail.com>
2017-06-02 16:36:20 -0300

    some fixes

--- Model.h
+++ Model.h
@@ -21,21 +21,18 @@ using namespace std;
 
 GLint TextureFromFile( const char *path, string directory );
 
-class Model
-{
+class Model{
+
 public:
     /*  Functions   */
     // Constructor, expects a filepath to a 3D model.
-    Model( GLchar *path )
-    {
-        this->loadModel( path );
+    Model(GLchar *path){
+        this->loadModel(path);
     }
     
     // Draws the model, and thus all its meshes
-    void Draw( Shader shader )
-    {
-        for ( GLuint i = 0; i < this->meshes.size( ); i++ )
-        {
+    void Draw(Shader shader){
+        for( GLuint i = 0; i < this->meshes.size( ); i++ ){
             this->meshes[i].Draw( shader );
         }
     }
@@ -48,15 +45,13 @@ private:
     
     /*  Functions   */
     // Loads a model with supported ASSIMP extensions from file and stores the resulting meshes in the meshes vector.
-    void loadModel( string path )
-    {
+    void loadModel(string path){
         // Read file via ASSIMP
         Assimp::Importer importer;
         const aiScene *scene = importer.ReadFile( path, aiProcess_Triangulate | aiProcess_FlipUVs );
-        
+       
         // Check for errors
-        if( !scene || scene->mFlags == AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode ) // if is Not Zero
-        {
+        if( !scene || scene->mFlags == AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode ){// if is Not Zero
             cout << "ERROR::ASSIMP:: " << importer.GetErrorString( ) << endl;
             return;
         }
@@ -64,15 +59,13 @@ private:
         this->directory = path.substr( 0, path.find_last_of( '/' ) );
         
         // Process ASSIMP's root node recursively
-        this->processNode( scene->mRootNode, scene );
+        this->processNode(scene->mRootNode, scene);
     }
     
     // Processes a node in a recursive fashion. Processes each individual mesh located at the node and repeats this process on its children nodes (if any).
-    void processNode( aiNode* node, const aiScene* scene )
-    {
+    void processNode( aiNode* node, const aiScene* scene){
         // Process each mesh located at the current node
-        for ( GLuint i = 0; i < node->mNumMeshes; i++ )
-        {
+        for ( GLuint i = 0; i < node->mNumMeshes; i++ ){
             // The node object only contains indices to index the actual objects in the scene.
             // The scene contains all the data, node is just to keep stuff organized (like relations between nodes).
             aiMesh* mesh = scene->mMeshes[node->mMeshes[i]];
@@ -81,22 +74,19 @@ private:
         }
         
         // After we've processed all of the meshes (if any) we then recursively process each of the children nodes
-        for ( GLuint i = 0; i < node->mNumChildren; i++ )
-        {
+        for ( GLuint i = 0; i < node->mNumChildren; i++ ){
             this->processNode( node->mChildren[i], scene );
         }
     }
     
-    Mesh processMesh( aiMesh *mesh, const aiScene *scene )
-    {
+    Mesh processMesh( aiMesh *mesh, const aiScene *scene){
         // Data to fill
         vector<Vertex> vertices;
         vector<GLuint> indices;
         vector<Texture> textures;
         
         // Walk through each of the mesh's vertices
-        for ( GLuint i = 0; i < mesh->mNumVertices; i++ )
-        {
+        for(GLuint i = 0; i < mesh->mNumVertices; i++){
             Vertex vertex;
             glm::vec3 vector; // We declare a placeholder vector since assimp uses its own vector class that doesn't directly convert to glm's vec3 class so we transfer the data to this placeholder glm::vec3 first.
             
@@ -113,8 +103,7 @@ private:
             vertex.Normal = vector;
             
             // Texture Coordinates
-            if( mesh->mTextureCoords[0] ) // Does the mesh contain texture coordinates?
-            {
+            if( mesh->mTextureCoords[0]){// Does the mesh contain texture coordinates?
                 glm::vec2 vec;
                 // A vertex can contain up to 8 different texture coordinates. We thus make the assumption that we won't
                 // use models where a vertex can have multiple texture coordinates so we always take the first set (0).
@@ -122,8 +111,7 @@ private:
                 vec.y = mesh->mTextureCoords[0][i].y;
                 vertex.TexCoords = vec;
             }
-            else
-            {
+            else{
                 vertex.TexCoords = glm::vec2( 0.0f, 0.0f );
             }
             
@@ -131,8 +119,7 @@ private:
         }
         
         // Now wak through each of the mesh's faces (a face is a mesh its triangle) and retrieve the corresponding vertex indices.
-        for ( GLuint i = 0; i < mesh->mNumFaces; i++ )
-        {
+        for ( GLuint i = 0; i < mesh->mNumFaces; i++ ){
             aiFace face = mesh->mFaces[i];
             // Retrieve all indices of the face and store them in the indices vector
             for ( GLuint j = 0; j < face.mNumIndices; j++ )
@@ -142,8 +129,7 @@ private:
         }
         
         // Process materials
-        if( mesh->mMaterialIndex >= 0 )
-        {
+        if( mesh->mMaterialIndex >= 0 ){
             aiMaterial* material = scene->mMaterials[mesh->mMaterialIndex];
             // We assume a convention for sampler names in the shaders. Each diffuse texture should be named
             // as 'texture_diffuseN' where N is a sequential number ranging from 1 to MAX_SAMPLER_NUMBER.
@@ -167,31 +153,25 @@ private:
     
     // Checks all material textures of a given type and loads the textures if they're not loaded yet.
     // The required info is returned as a Texture struct.
-    vector<Texture> loadMaterialTextures( aiMaterial *mat, aiTextureType type, string typeName )
-    {
+    vector<Texture> loadMaterialTextures( aiMaterial *mat, aiTextureType type, string typeName ){
         vector<Texture> textures;
         
-        for ( GLuint i = 0; i < mat->GetTextureCount( type ); i++ )
-        {
+        for ( GLuint i = 0; i < mat->GetTextureCount( type ); i++ ){
             aiString str;
             mat->GetTexture( type, i, &str );
             
             // Check if texture was loaded before and if so, continue to next iteration: skip loading a new texture
             GLboolean skip = false;
             
-            for ( GLuint j = 0; j < textures_loaded.size( ); j++ )
-            {
-                if( textures_loaded[j].path == str )
-                {
+            for ( GLuint j = 0; j < textures_loaded.size( ); j++ ){
+                if( textures_loaded[j].path == str ){
                     textures.push_back( textures_loaded[j] );
-                    skip = true; // A texture with the same filepath has already been loaded, continue to next one. (optimization)
-                    
+                    skip = true; // A texture with the same filepath has already been loaded, continue to next one. (optimization)         
                     break;
                 }
             }
             
-            if( !skip )
-            {   // If texture hasn't been loaded already, load it
+            if(!skip){   // If texture hasn't been loaded already, load it
                 Texture texture;
                 texture.id = TextureFromFile( str.C_Str( ), this->directory );
                 texture.type = typeName;
@@ -217,6 +197,7 @@ GLint TextureFromFile( const char *path, string directory )
     int width, height;
     
     unsigned char *image = SOIL_load_image( filename.c_str( ), &width, &height, 0, SOIL_LOAD_RGB );
+    
     
     // Assign texture to ID
     glBindTexture( GL_TEXTURE_2D, textureID );
--- main.cpp
+++ main.cpp
@@ -39,47 +39,44 @@ bool firstMouse = true;
 GLfloat deltaTime = 0.0f;
 GLfloat lastFrame = 0.0f;
 
-int main( )
-{
+int main(){
 	
 	double lastTime = glfwGetTime();
 	int nbFrames = 0;
     // Init GLFW
-    glfwInit( );
+    glfwInit();
     // Set all the required options for GLFW
-    glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 3 );
-    glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 3 );
-    glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
-    glfwWindowHint( GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE );
-    glfwWindowHint( GLFW_RESIZABLE, GL_FALSE );
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
     
     // Create a GLFWwindow object that we can use for GLFW's functions
     GLFWwindow *window = glfwCreateWindow( WIDTH, HEIGHT, "LearnOpenGL", NULL, NULL );
     
-    if ( NULL == window )
-    {
+    if (NULL == window){
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate( );
         
         return EXIT_FAILURE;
     }
     
-    glfwMakeContextCurrent( window );
+    glfwMakeContextCurrent(window);
     
-    glfwGetFramebufferSize( window, &SCREEN_WIDTH, &SCREEN_HEIGHT );
+    glfwGetFramebufferSize(window, &SCREEN_WIDTH, &SCREEN_HEIGHT);
     
     // Set the required callback functions
-    glfwSetKeyCallback( window, KeyCallback );
-    glfwSetCursorPosCallback( window, MouseCallback );
+    glfwSetKeyCallback(window, KeyCallback);
+    glfwSetCursorPosCallback( window, MouseCallback);
     
     // GLFW Options
-    glfwSetInputMode( window, GLFW_CURSOR, GLFW_CURSOR_DISABLED );
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
     
     // Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
     glewExperimental = GL_TRUE;
     // Initialize GLEW to setup the OpenGL Function pointers
-    if ( GLEW_OK != glewInit( ) )
-    {
+    if(GLEW_OK != glewInit()){
         std::cout << "Failed to initialize GLEW" << std::endl;
         return EXIT_FAILURE;
     }
@@ -95,17 +92,16 @@ int main( )
     
     // Load models
     //Model ourModel1( "res/models/catherham/Catherham_Seven_Sigma_OBJ.obj" );
-    Model ourModel1("res/models/Roads/Roads.obj");
+    Model ourModel2("res/models/Ferrari/ferrari-f1-race-car.obj");
     // Load models
-	Model ourModel2( "res/models/nanosuit/nanosuit.obj" );
+	Model ourModel1( "res/models/item01/Item01.obj" );
     // Draw in wireframe
-    glPolygonMode( GL_FRONT_AND_BACK, GL_LINE );
+    //glPolygonMode( GL_FRONT_AND_BACK, GL_LINE );
     
     glm::mat4 projection = glm::perspective( camera.GetZoom( ), ( float )SCREEN_WIDTH/( float )SCREEN_HEIGHT, 0.1f, 500.0f );
     
     // Game loop
-    while( !glfwWindowShouldClose( window ) )
-    {
+	while( !glfwWindowShouldClose( window )){
 		
 		// Measure speed
 		double currentTime = glfwGetTime();
@@ -139,17 +135,16 @@ int main( )
         
         //Draw the loaded model
         glm::mat4 model1,model2;
-        model1 = glm::translate( model1, glm::vec3( 0.0f, 0.0f, 0.0f ) ); // Translate it down a bit so it's at the center of the scene
-        model1 = glm::scale( model1, glm::vec3( 0.2f, 0.2f, 0.2f ) );	// It's a bit too big for our scene, so scale it down
+        model1 = glm::translate( model1, glm::vec3( 0.0f, 0.0f, 0.0f)); // Translate it down a bit so it's at the center of the scene
+        model1 = glm::scale( model1, glm::vec3( 0.2f, 0.2f, 0.2f));	// It's a bit too big for our scene, so scale it down
         glUniformMatrix4fv( glGetUniformLocation( shader.Program, "model" ), 1, GL_FALSE, glm::value_ptr( model1 ) );
         ourModel1.Draw( shader );
-        
+       
         //Draw the second loeaded model
-        model2 = glm::translate( model2, glm::vec3( -1.5f, 0.0f, 0.0f ) ); // Translate it down a bit so it's at the center of the scene
-        model2 = glm::rotate(model2, -5.0f, glm::vec3( 0, 1, 0 ));
-        model2 = glm::scale( model2, glm::vec3( 0.15f, 0.15f, 0.15f ) );	// It's a bit too big for our scene, so scale it down
-        
-        
+        model2 = glm::translate( model2, glm::vec3( 7.0f, 0.0f, 0.0f)); // Translate it down a bit so it's at the center of the scene
+        model2 = glm::rotate(model2, 0.0f, glm::vec3( 0, 1, 0 ));
+        model2 = glm::scale( model2, glm::vec3( 1.0f, 1.0f, 1.0f));	// It's a bit too big for our scene, so scale it down
+      
         glUniformMatrix4fv( glGetUniformLocation( shader.Program, "model" ), 1, GL_FALSE, glm::value_ptr( model2 ) );
         ourModel2.Draw( shader );
         
@@ -162,55 +157,43 @@ int main( )
 }
 
 // Moves/alters the camera positions based on user input
-void DoMovement( )
-{
+void DoMovement( ){
     // Camera controls
-    if ( keys[GLFW_KEY_W] || keys[GLFW_KEY_UP] )
-    {
-        camera.ProcessKeyboard( FORWARD, deltaTime );
+    if(keys[GLFW_KEY_W] || keys[GLFW_KEY_UP]){
+        camera.ProcessKeyboard( FORWARD, deltaTime);
     }
     
-    if ( keys[GLFW_KEY_S] || keys[GLFW_KEY_DOWN] )
-    {
+    if(keys[GLFW_KEY_S] || keys[GLFW_KEY_DOWN]){
         camera.ProcessKeyboard( BACKWARD, deltaTime );
     }
     
-    if ( keys[GLFW_KEY_A] || keys[GLFW_KEY_LEFT] )
-    {
-        camera.ProcessKeyboard( LEFT, deltaTime );
+    if (keys[GLFW_KEY_A] || keys[GLFW_KEY_LEFT]){
+        camera.ProcessKeyboard( LEFT, deltaTime);
     }
     
-    if ( keys[GLFW_KEY_D] || keys[GLFW_KEY_RIGHT] )
-    {
+    if(keys[GLFW_KEY_D] || keys[GLFW_KEY_RIGHT]){
         camera.ProcessKeyboard( RIGHT, deltaTime );
     }
 }
 
 // Is called whenever a key is pressed/released via GLFW
-void KeyCallback( GLFWwindow *window, int key, int scancode, int action, int mode )
-{
-    if ( GLFW_KEY_ESCAPE == key && GLFW_PRESS == action )
-    {
+void KeyCallback( GLFWwindow *window, int key, int scancode, int action, int mode ){
+    if(GLFW_KEY_ESCAPE == key && GLFW_PRESS == action){
         glfwSetWindowShouldClose(window, GL_TRUE);
     }
     
-    if ( key >= 0 && key < 1024 )
-    {
-        if ( action == GLFW_PRESS )
-        {
+    if(key >= 0 && key < 1024){
+        if ( action == GLFW_PRESS ){
             keys[key] = true;
         }
-        else if ( action == GLFW_RELEASE )
-        {
+        else if (action == GLFW_RELEASE){
             keys[key] = false;
         }
     }
 }
 
-void MouseCallback( GLFWwindow *window, double xPos, double yPos )
-{
-    if ( firstMouse )
-    {
+void MouseCallback( GLFWwindow *window, double xPos, double yPos ){
+    if (firstMouse){
         lastX = xPos;
         lastY = yPos;
         firstMouse = false;
@@ -222,6 +205,6 @@ void MouseCallback( GLFWwindow *window, double xPos, double yPos )
     lastX = xPos;
     lastY = yPos;
     
-    camera.ProcessMouseMovement( xOffset, yOffset );
+    camera.ProcessMouseMovement(xOffset,yOffset);
 }
 
